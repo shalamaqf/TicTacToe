85682d1cba27d7af04bd3ddf3ad947d17b1aa26b
Shalama <shalamafadillaa@gmail.com>
2025-10-07 11:10:41 +0700

    Add comments

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Create a module pattern to wrap logic of the game board
 var GameBoard = ( function () {
+    // Create a board made from a 2D array
     let board = [
         [1, 2, 3],
         [4, 5, 6],
         [7, 8, 9]
     ];
 
+    // Create a function to display the board to the console
     const displayBoard = function () {
         let i;
         for (i = 0; i < 3; i++) {
@@ -12,6 +15,7 @@ var GameBoard = ( function () {
         }
     }
 
+    // Create a function to reset the board
     const resetBoard = function () {
         board = [
             [1, 2, 3],
