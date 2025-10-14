31283d7ebd1b73c72d8c2e25c54cefccfe47284a
Shalama <shalamafadillaa@gmail.com>
2025-10-14 11:19:04 +0700

    Fix function call

--- script.js
+++ script.js
@@ -287,7 +287,7 @@ const screenController = ( function () {
         // Attach an event to the button
         startBtn.addEventListener('click', function () {
             clearScreen();
-            renderNameInputForm();
+            continueButtonSetup();
         })
     }
 
